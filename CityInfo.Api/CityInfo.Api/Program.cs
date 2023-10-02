03eb987d41a0bae244d0e74f56ef659cb941e4e7
Kavit Trivedi <kavittrivedi@hotmail.com>
2023-10-02 11:50:02 +0530

    Now data will return in json and xml.

--- CityInfo.Api/CityInfo.Api/Program.cs
+++ CityInfo.Api/CityInfo.Api/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options=>
+{ 
+    options.ReturnHttpNotAcceptable=true;
+}).AddXmlDataContractSerializerFormatters(); // If you want output in xml.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
