c1a07df062dfd35e586baac237873e49bcf80023
Kavit Trivedi <kavittrivedi@hotmail.com>
2023-10-03 23:24:58 +0530

    Add 2 new nuget packages.

--- CityInfo.Api/CityInfo.Api/Program.cs
+++ CityInfo.Api/CityInfo.Api/Program.cs
@@ -8,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options=>
 { 
     options.ReturnHttpNotAcceptable=true;
-}).AddXmlDataContractSerializerFormatters(); // If you want output in xml.
+}).AddNewtonsoftJson()
+  .AddXmlDataContractSerializerFormatters(); // If you want output in xml.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
